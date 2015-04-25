8958601059e78092a62d20f1c9e56f8d15b3c8fb
Jonathan Bodemann <jonathan.bodemann@student.htldornbirn.at>
2015-04-25 21:47:41 +0200

    forgot to make the unused player invisible

--- proxerhtml5.user.js
+++ proxerhtml5.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Proxer HTML5 Player
 // @namespace    https://github.com/Dragowynd/ProxerHTML5
-// @version      2.0.1
+// @version      2.0.2
 // @description  Improves the Proxer.ME HTML5 Player
 // @updateURL    https://raw.githubusercontent.com/Dragowynd/ProxerHTML5/master/proxerhtml5.user.js
 // @downloadURL  https://raw.githubusercontent.com/Dragowynd/ProxerHTML5/master/proxerhtml5.user.js
@@ -16,9 +16,13 @@ document.body.onload = function () {
 	loadStylesheet("http://vjs.zencdn.net/4.12/video-js.css");
 	loadScript("http://vjs.zencdn.net/4.12/video.js", function () {
 		loadScript("http://cdn.sc.gl/videojs-hotkeys/latest/videojs.hotkeys.min.js", function () {
+
 			var srctag = document.querySelector("#player_code .flowplayer video").firstElementChild;
 			var src = srctag.getAttribute("src");
 			var type = srctag.getAttribute("type");
+
+			document.getElementById("player_code").setAttribute("style", "display: none;");
+
 			var video = document.createElement("video");
 			video.id = "htmlstream";
 			video.setAttribute("class", "video-js vjs-default-skin vjs-big-play-centered");
@@ -32,6 +36,7 @@ document.body.onload = function () {
 			source.setAttribute("src", src);
 			source.setAttribute("type", type);
 			video.appendChild(source);
+
 			document.body.appendChild(video);
 
 			videojs("htmlstream", {}, function () {}).ready(function () {
